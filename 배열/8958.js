34f5b53123febf76c26c7d8859abb07265d79e69
paduck-96 <peterrabitt1234@naver.com>
2022-12-10 16:29:00 +0900

    feat: 백준 8958

--- 배열/8958.js
+++ 배열/8958.js
@@ -1,23 +1,23 @@
-const fs = require('fs');
+const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const time = Number(input[0]);
 let totalScore = 0;
 let checkScore = 0;
 let result = [];
 let count = 1;
-while(count<=time){
-    for(let num = 0; num<input[count].length-1;num++){
-        if(input[count][num]==="O"){
-            checkScore+=1;
-            }
-        if(input[count][num]==="X"){
-            checkScore=0;
-            }
-        totalScore += checkScore;
+while (count <= time) {
+  let aLength = input[count].length;
+  for (let num = 0; num < aLength; num++) {
+    if (input[count][num] === "O") {
+      checkScore += 1;
+    } else {
+      checkScore = 0;
     }
-    count++;
-    result.push(totalScore);
-    totalScore = 0;
-    checkScore=0;
+    totalScore += checkScore;
+  }
+  count++;
+  result.push(totalScore);
+  totalScore = 0;
+  checkScore = 0;
 }
 console.log(result.join("\n"));
