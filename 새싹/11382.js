e58db17b4412f595ade641f41ca380d88899ec53
paduck-96 <peterrabitt1234@naver.com>
2022-12-10 16:41:51 +0900

    feat: 백준 11382

---
+++ 새싹/11382.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((n) => Number(n));
+
+console.log(input[0] + input[1] + input[2]);
