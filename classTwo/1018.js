1b6c26459c12b466f371dd760c33e9eb580a120b
paduck-96 <peterrabitt1234@naver.com>
2022-12-18 00:55:11 +0900

    feat:백준 1018 참고본

--- classTwo/1018.js
+++ classTwo/1018.js
@@ -1,81 +1,75 @@
 const fs = require("fs");
 const input = fs.readFileSync("sample.txt").toString().trim().split("\n");
-const [height, width] = input[0].split(" ").map((n) => Number(n));
+// shift 메서드를 활용해 input[0]의 조건 출력하고
+//문제로 주어지는 W,B의 값을 배열로 저장한다.
+const [height, width] = input.shift().split(" ").map(n=>Number(n));
 
-let toggleCount = [];
-// W,B 토글
-const toggle = (letters)=>{
-  let count = 0;
-  let check = letters.length-1;
-  let arr = [...letters];
-  let start = 0;
-  if(arr[start]!=arr[check]){
-    let wlength = arr.filter(e=>e==="W").length;
-    let blength = arr.filter(e=>e==="B").length;
-    if(wlength>blength){
-      if(arr[start]!="B"){arr[start]="B";
-      count+=1;
+const minArr = [];
+
+// 정답을 미리 생성한 후
+//각 자리를 비교하는 방법이
+//브루트포스 알고리즘에서 가정 최적의 방법인듯?
+
+const whiteFirst = [
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW'
+    ];
+     
+    const blackFirst = [
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB'
+    ];
+
+    // 보드판과 비교 함수
+    //
+const whiteFirstBoard = (height, width) => {
+    // 변경 횟수 계산
+    let counter = 0;
+
+    // 높이 기준 비교
+    //8행을 가지기 때문에 8번
+    for(let i=height;i<height+8;i++){
+        //8열을 가지기 때문에 8번
+        for(let j=width;j<width+8;j++){
+            //현재 input에는 문제에서 주어지는 전체 보드판 들어가있음
+            //함수 매개변수로 8개씩 돌 판의 범위가 주어짐
+            //각 판의 첫 행 과 첫 열
+            if(input[i][j] !== whiteFirst[i-height][j-width])counter++;
+        }
     }
-      if(arr[check]!="B"){
-        arr[check]="B";
-        count+=1;
-      }
-    }else if(wlength<blength){
-      if(arr[start]!="W"){arr[start]="W";
-      count+=1;
+    return counter;
+}
+const blackFirstBoard = (height, width) => {
+    let counter = 0;
+    for(let i=height;i<height+8;i++){
+        for(let j=width;j<width+8;j++){
+            if(input[i][j] !== whiteFirst[i-height][j-width])counter++;
+        }
     }
-      if(arr[check]!="W"){
-        arr[check]="W";
-        count+=1;
-      }
-    }
-  }
-  while(start<check){
-    if(start%8!==7){
-      if(arr[start]==arr[start+1]){
-        if(arr[start] == "W")arr[start+1]="B";
-        if(arr[start]=="B")arr[start+1]="W";
-        count+=1;
-      }
-      if(arr[start]==arr[start+8]){
-        if(arr[start] == "W")arr[start+8]="B";
-        if(arr[start]=="B")arr[start+8]="W";
-        count+=1;
-      }
-    }else{
-      
-    }
-    start++;
-  }
-  toggleCount.push(count);
-  return toggleCount;
+    return counter;
 }
 
-// 8x8 보드 찍어내기
-let checkBoard = [];
-let result ="";
-let heightIndex = 0;
-
-for(let heightCheck = 0; heightCheck<=height-8; heightCheck++){
-  for(let widthCheck = 0; widthCheck<=width-8; widthCheck++){
-    heightIndex = 0;
-    result="";
-    while(heightIndex<8){
-      result +=input[heightIndex+heightCheck+1].substr(widthCheck,8)
-      heightIndex++;
+// 모든 경우의 수에 따른 확인 진행
+//문제에서 주어지는 전체 보드에서 8x8을 추출하는 것이기 때문에
+//행, 열에서 8개를 빼면 각 행, 열이 몇 번씩 옮겨져야하는지 알 수 있음
+//count를 확인해야하는 보드 경우의 수
+for(let i =0; i+7<height;i++){
+    for(let j=0; j+7<width;j++){
+        minArr.push(whiteFirstBoard(i,j));
+        minArr.push(blackFirstBoard(i,j))
     }
-    checkBoard.push(result);
-  }
-  }
-
-  // 각 보드를 확인하기
-  let checkBoardLength = checkBoard.length;
-  let time = 0;
-  let answer;
-  while(time<checkBoardLength){
-    answer = toggle(checkBoard[time]);
-    time++;
 }
 
-//결과
-console.log(Math.min(...answer));
+console.log(Math.min.apply(null, minArr));
