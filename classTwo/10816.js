6a9beaf3a629ccd34efe91847b1f692237879b4d
강지덕 <jideok@gangjideog-ui-MacBookAir.local>
2023-01-18 14:07:18 +0900

    rename: 백준 10816 시간초과 map으로 해결

--- classTwo/10816.js
+++ classTwo/10816.js
@@ -4,13 +4,17 @@ const [totalCard, totalCardList, sgCard, sgCardList] = fs.readFileSync("/dev/std
 const totalArr = totalCardList.split(" ");
 const sgArr = sgCardList.split(" ");
 const result = [];
-sgArr.forEach(el => {
-  let num = 0;
-  totalArr.forEach(check => {
-    if(el == check) {
-      num+=1;
-    }
-  })
-  result.push(num);
+// 중복을 제거한 값의 목록을 만들기 위한 과정
+//map은 키 와 value 로 저장해 편리
+const totalMap = new Map();
+totalArr.forEach(el=>{
+  if(totalMap.has(el))totalMap.set(el, totalMap.get(el)+1);
+  else totalMap.set(el, 1);
 })
+// 중복 제거된 map 과 비교해 결과 확인
+sgArr.forEach(el => {
+  if(totalMap.has(el)) result.push(totalMap.get(el));
+  else result.push(0);
+})
+
 console.log(result.join(" "));
