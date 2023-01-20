d97227a1bc66e92df3725bc11c86f743a1fb7e7c
강지덕 <jideok@gangjideog-ui-MacBookAir.local>
2023-01-20 23:49:34 +0900

    feat: 백준 10866

---
+++ classTwo/10866.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const requestTime = Number(input.shift());
+let start = 0;
+let result = [];
+let answer = [];
+while (start < requestTime) {
+  const arr = input[start].trim().split(" ");
+  let work, time;
+  if (arr.length == 2) {
+    work = arr[0];
+    time = arr[1];
+  } else {
+    work = arr[0];
+  }
+  switch (work) {
+    case "push_front":
+      result.unshift(time);
+      break;
+      case "push_back":
+        result.push(time);
+        break;
+    case "pop_front":
+      let popFrontNum = result.shift();
+      if (popFrontNum == undefined) {
+        answer.push(-1);
+      } else answer.push(popFrontNum)
+      break;
+      case "pop_back":
+        let popBackNum = result.pop();
+        if(popBackNum == undefined){
+          answer.push(-1);
+        }else answer.push(popBackNum);
+        break;
+    case "size":
+      answer.push(result.length);
+      break;
+    case "empty":
+      if (result.length == 0) {
+        answer.push(1);
+      } else {
+        answer.push(0)
+      }
+      break;
+    case "front":
+      if(result[0]===undefined){
+        answer.push(-1)
+      }else answer.push(result[0])
+      break;
+      case "back":
+        if(result[result.length-1]===undefined){
+          answer.push(-1)
+        }else answer.push(result[result.length-1]);
+        break;
+    default:
+      return;
+  }
+  start++;
+}
+console.log(answer.join("\n"));
