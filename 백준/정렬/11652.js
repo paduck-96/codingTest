909c27a747665b223e691a4c3eda01460eee03a4
paduck-96 <peterrabitt1234@naver.com>
2023-03-13 23:29:49 +0900

    feat: 백준 11652 map 으로 수정

--- 백준/정렬/11652.js
+++ 백준/정렬/11652.js
@@ -2,20 +2,21 @@ const fs = require('fs');
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./samples.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n").map(BigInt);
 
-let cardsMap = new Map();
+let cardsSet = new Map();
 
-for(let i =0; i<input.length; i++){
-    cardsMap.get(input[i])?cardsMap.set(input[i], cardsMap.get(input[i])+1):cardsMap.set(input[i], 1)
-}
+input.forEach(el=>{
+    cardsSet.has(el)?cardsSet.set(el, cardsSet.get(el)+1):cardsSet.set(el, 1);
+})
 
-let result = [...cardsMap].sort((a,b)=>{
-    if(b[1]<a[1])return -1;
+let cardSetArr = [...cardsSet];
+cardSetArr.sort((a,b)=>{
+    if(b[1]<a[1]) return -1;
     else if(b[1]>a[1]) return 1;
     else{
-        if (a[0] < b[0]) return -1;
-        if (a[0] > b[0]) return 1;
+        if(a[0]<b[0])return -1;
+        if(a[0]>b[0])return 1;
         else return 0;
     }
 })
 
-console.log(result[0][0].toString())
+console.log(cardSetArr[0][0].toString())
