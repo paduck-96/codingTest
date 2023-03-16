cb3a2a30f1a495ba0f4cc81e0f4d30eafcd26dcc
paduck_96 <rkdwlejr123@gmail.com>
2023-03-16 10:50:55 +0900

    [Bronze I] Title: ROT13, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./samples.txt";
+const inputs = fs.readFileSync(filePath).toString();
+
+var answer = '';
+for(var i=0; i<inputs.length; i++){
+    var char = inputs[i].charCodeAt(0);
+    if(char>=65 && char<=90){
+        var upper = char+13;
+        if(upper>90){
+            answer += String.fromCharCode(upper-26);
+        }else{
+            answer += String.fromCharCode(upper);
+        }
+    }else if(char >= 97 && char <= 122){
+        var lower = char+13;
+        if(lower >122){
+            answer += String.fromCharCode(lower-26);
+        }else{
+            answer += String.fromCharCode(lower);
+        }
+    }else if(char === 32){
+        answer += ' ';
+    }else{
+        answer += inputs[i];
+    }
+}
+console.log(answer);
